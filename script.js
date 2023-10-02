e426bdd1fa336e0a1e4965d8bb6ba8033a436a4c
arbaazalishaikh <arbaazalishaikh73@gmail.com>
2023-10-02 12:33:16 +0530

    update

--- script.js
+++ script.js
@@ -15,13 +15,25 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveDate()
 }
 
 listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveDate()
     }
     else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveDate()
     }
 }, false)
+
+function saveDate(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
